28b917d281e9a98213676e5857b746e217b1780a
xiaojigugu <562015052@qq.com>
2021-04-20 17:48:12 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dataoke
-大淘客SDK
+# DaTaoKe
+
+大淘客Node版本SDK。基于大淘客v1.0.0的API版本。
+
+## Getting start
+
+```bash
+$ npm install dataoke
+```
+
+## DEMO
+
+```js
+  let DaTaoKe = require("dataoke");
+  let daTaoke = new DaTaoKe("你的大淘客appKey", "你的大淘客appSecret");
+
+  //第一个参数是接口地址，第二个参数是除公共参数以外的其他参数
+  let data = await daTaoke.request("tb-service/activity-link", {
+    promotionSceneId: "20150318020002192",
+  })
+
+  console.log(data)
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const crypto = require('crypto');
+const md5 = crypto.createHash('md5');
+
+class dataoke {
+
+  /**初始化
+   * @param {String} appKey 在大淘客平台上申请得到的appKey
+   * @param {String} appSecret 在大淘客平台上申请得到的appSecret
+   */
+  constructor(appKey, appSecret) {
+    this.appKey = appKey;
+    this.appSecret = appSecret;
+    this.version = 'v1.0.0'; // API接口版本号
+  }
+
+  /**验签sign参数加密
+   * @param data
+   * @param appSecret
+   * @return string
+   */
+  makeSign(data) {
+    let str = '';
+    let index = 0;
+    let sortPor = [];
+
+    for (let key in data) {
+      sortPor.push(`${key}=${data[key]}`);
+    }
+    // 排序
+    sortPor.sort();
+
+    // 转url
+    for (let key in sortPor) {
+      str = `${str}${index === 0 ? '' : '&'}${sortPor[key]}`;
+      index++;
+    }
+
+    // md5加密
+    const ret = md5.update(`${str}&key=${this.appSecret}`).digest('hex');
+
+    return ret;
+  }
+
+  /**请求大淘客获得数据
+   * @param api 请求接口 如："tb-service/activity-link"
+   * @param options 除公共参数以外的其他参数
+   * @return string
+   */
+  request(api, options) {
+    let data = {
+      version: this.version, // API接口版本号
+      appKey: this.appKey, // 应用分配的appKey
+    };
+    Object.keys(options).forEach((key) => {
+      data[key] = options[key];
+    })
+    data.sign = this.makeSign(data);
+
+    return new Promise((resolve, reject) => {
+      axios({
+        url: `https://openapi.dataoke.com/api/${api}`, //请求地址
+        method: 'GET',
+        params: data
+      }).then(({data}) => {
+        resolve(data);
+      }).catch((err) => {
+        resolve({
+          code: -1001,
+          msg: err || "网络请求失败或者其他错误"
+        });
+      });
+    })
+  }
+}
+
+module.exports = dataoke;
